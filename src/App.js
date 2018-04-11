64c2dfb934cb90940abf1de786740c2c8c1017ac
Faïssal <fhattou@simplon.co>
2018-04-11 12:33:03 +0200

    Initial commit :fire:

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
-import avocat from './avocat.gif';
-
-import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <p>React here!</p>
-        <img src={avocat}/>
+        <p>CompteFacile</p>
       </div>
     );
   }
